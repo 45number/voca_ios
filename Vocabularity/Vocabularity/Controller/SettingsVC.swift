792016a70f67ec96839844fcf6f3482bf0670061
Admin <admin@Mac-Admin.local>
2018-10-06 17:47:07 +0300

    ok

--- Vocabularity/Vocabularity/Controller/SettingsVC.swift
+++ Vocabularity/Vocabularity/Controller/SettingsVC.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class SettingsVC: UIViewController {
 
+    
+    //Variables
+    private let dataSource = ["Apple", "Microsoft", "Samsung", "Android", "Google"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        
     }
     
     //Actions
@@ -20,5 +26,55 @@ class SettingsVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func wordsAtTimePressed(_ sender: Any) {
+        createWordsAtTimeAlertView()
+    }
+    
+    func createWordsAtTimeAlertView() {
+        
+        
+        let alert = UIAlertController(title: "Add", message: "What to add:", preferredStyle: UIAlertControllerStyle.alert)
+        
+        
+        
+        
+//        let alert = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
+//
+        let pickerFrame = CGRect(x: 17, y: 52, width: 270, height: 100)
+        let picker = UIPickerView(frame: pickerFrame)
+        
+        picker.delegate = self
+        picker.dataSource = self
+        
+        alert.view.addSubview(picker)
+//
+//        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
+//
+//        alert.addAction(action)
+//        present(alert, animated: true, completion: nil)
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: { action in }))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+}
+
+extension SettingsVC: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+//        detailLabel.text = dataSource[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataSource[row]
+    }
     
 }

d48e343bd72a45b0a5e2da6bddf360dc205b9fd7
Admin <admin@Mac-Admin.local>
2018-10-24 15:19:36 +0300

    ok

---
+++ Vocabularity/Vocabularity/CustomAlertView/TutorialExcel.swift
@@ -0,0 +1,19 @@
+//
+//  TutorialExcel.swift
+//  Vocabularity
+//
+//  Created by Admin on 24.10.2018.
+//  Copyright © 2018 vocabularity. All rights reserved.
+//
+
+import UIKit
+
+class TutorialExcel: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+}
